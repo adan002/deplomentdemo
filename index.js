04d1062c274eef9e2e435cbb0260c577d29daed6
adan villalpando <adan_1990.icloud.com>
2022-06-30 16:17:02 -0600

    3

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const message = () => {
 }
 
 bigButton.addEventListener('click', message)
+
--- server/index.js
+++ server/index.js
@@ -1,24 +1,32 @@
 const express = require('express')
 const path = require('path')
-
-
 const app = express()
 
-app.get('/', ( req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'))
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '27958146dcf142b2a6b34ead2a27e05e',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
 })
 
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+
 app.get('/css',(req,res) => {
-    res.sendFile(path.join(__dirname, '../styles.css'))
-  })
-  
-  app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.js'))
-  })
+  res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+app.get('/js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.js'))
+})
 
 const port = process.env.PORT || 4005
 
-
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
